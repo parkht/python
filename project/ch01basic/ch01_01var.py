6fb874d396a9d33635ff240979a334a89d7ac07c
parkht <59907071+parkht@users.noreply.github.com>
2020-01-17 11:31:37 +0900

    ch01

---
+++ project/ch01basic/ch01_01var.py
@@ -0,0 +1,16 @@
+
+# a = 100
+# b = 200
+# input() - 사용자에게 문자열로 데이터를 입력 받는다.
+a = int(input('첫번째 숫자를 입력해주세요 : '))
+b = int(input('두번째 숫자를 입력해수세요 : '))
+result = a+b
+print(a, '+', b, '=', result)
+result = a-b
+print(a, '-', b, '=', result)
+result = a*b
+print(a, '*', b, '=', result)
+result = a/b
+print(a, '/', b, '=', result)
+
+
