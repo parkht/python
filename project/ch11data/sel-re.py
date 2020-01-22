64ddc04ecb82eeae150c15574e5be73c53c3ebc7
parkht <59907071+parkht@users.noreply.github.com>
2020-01-22 12:36:22 +0900

    요소 추출(2020.01.22)

---
+++ project/ch11data/sel-re.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import re
+
+html = """
+<ul>
+  <li><a href='hoge.html'>hoge</li>
+  <li><a href='https://example.com/fuga'>fuga</li>
+  <li><a href='https://example.com/foo'>foo</li>
+  <li><a href='http://example.com/aaa'>aaa</li>
+</ul>
+"""
+
+soup = BeautifulSoup(html,'html.parser')
+
+# 정규표현식으로 href에서 https인 것 추출하기
+li = soup.find_all(href=re.compile(r'^https://|http://'))
+for e in li: print(e.attrs['href'])
---
+++ project/ch11data/sel_avocado.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+fp = open("fruit_vegetables.html",encoding='utf-8')
+soup = BeautifulSoup(fp,'html.parser')
+
+# CSS 선택자로 추출하기
+# print(soup)
+#print(soup.select_one('li:nth-of-type()')) => 모든 li에서 찾는 것이 아니라 그룹들마다 li를 찾고 그룹별로 li순서를 정한다.
+# 그룹이 바뀌면 순서는 다시 1로 시작하기 때문에 'a'그룹의 첫번째 'li'와 'b' 그룹의 첫번째 'li'가 중복이 되서
+# 'a'그룹의 첫번째 'li'만 출력이 된다.
+print(soup.select_one('#ve-list > li:nth-of-type(4)').string)
+print(soup.select("#ve-list > li[data-lo='us']")[1].string)
+print(soup.select("#ve-list > li.black")[1].string)
+
+# find 메서드로 추출하기
+cond = {'data-lo':'us', 'class':'black'}
+print(soup.find('li', cond).string)
+
+# find 메서드를 연속적으로 사용하기
+print(soup.find(id='ve-list').find('li',cond).string)
